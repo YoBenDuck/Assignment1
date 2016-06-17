d3c9cfcd18fcaa68c38ffc70a0e4db395ba1650d
Ben <YoBenDuck@gmail.com>
2016-06-16 18:19:39 -0700

    started assignment1

---
+++ script.js
@@ -0,0 +1,23 @@
+//STEP1
+
+/* var someMonth;
+function theMonth;
+currentMonth;
+var summerMonth;
+myLibraryaFunction;*/
+
+//STEP3
+
+
+//STEP6
+var first = "My score is:" + 500;
+console.log(first);
+
+var dog = true + " My dog is nice";
+console.log(dog); 
+
+var cat = 500 + false;
+
+//STEP
+
+
