95b777dc0bfe16716f2bb4f5193e7dd69b51ab0e
Ben <YoBenDuck@gmail.com>
2016-06-18 16:48:49 -0700

    completed assignment1

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //STEP1
-
 /* 
 var someMonth;
 function theMonth;
@@ -9,7 +8,6 @@ myLibraryAFunction;
 */
 
 //STEP2
-
 /*
 3.295e10
 "My name is Ben"
@@ -17,10 +15,17 @@ true
 null
 */
 
-//STEP4
+//STEP3
+/*
+var FirstExample = 10 * (4 + 6);
+var SecondExample = FirstExample + "%" + 10;
+window.console.log(FirstExample);
+*/
 
+//STEP4
+/*
 var strFirstName;  
-var strLastName; 
+var strLastName;
 var strAddress;
 var strCity;
 var strState;
@@ -28,25 +33,175 @@ var strZipCode;
 var strYourAge;
 var strReferralSource;
 var strMayWeContactYou;
+*/
 
 //STEP5
+/*
+var strFirstName;
+var strLastName;
+var strYourAge;
+strFirstName = "Ben";
+strLastName = "Espinoza";
+strYourAge = 555;
+
+var strYourAge = 555;
+
+var strZipCode = 92110, strState = "CA";
+*/
 
 //STEP6
-
 /*
 var first = "My score is:" + 500;
-console.log(first);
+window.console.log(first);
 
 var dog = true + " My dog is nice";
-console.log(dog); 
+window.console.log(dog); 
 
 var cat = 500/false;
-console.log(cat);
+window.console.log(cat);
 */
 
 //STEP7
-
 /* declare variables */
+
+/*
+var dog = "animal";
+window.console.log("The dog is a " + dog + " " + breed + ".");
+var breed = "Dogo";
+*/
+
+//STEP8
+/*
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+window.console.log(someString);
+*/
+
+//STEP9
+/*
+x = null;
+window.console.log(x);
+*/
+
+//STEP10
+/*
+window.console.log(typeof "Ben");
+window.console.log(typeof 5);
+window.console.log(typeof true);
+typeof new Date()
+typeof myDog
+*/
+
+//STEP11
+/*
+alert("Hello Ben Espinoza," + "welcome to the JavaScript class!")
+*/
+
+//STEP12
+/*
+var name = "Ben Espinoza,";
+alert("Hello " + name + " welcome to the JavaScript class!")
+*/
+
+//STEP13
+/*
+var name = "Benjamin Espinoza,";
+var course = "JavaScript";
+alert("Hello " + name + " welcome to the " + course + " class!")
+*/
+
+//STEP14
+/*
+var name = "Ben Espinoza";
+var course = "JavaScript";
+alert("Hello " + name + ".\nWelcome to the " + course + " class!")
+*/
+
+//STEP15
+/*
+prompt("Please enter your name.","Ben Espinoza");
+*/
+
+//STEP16
+/*
+prompt("Which class are you taking?","JavaScript");
+*/
+
+//STEP17
+/*
+var x=10;
+var y=20;
+alert(x+y)
+*/
+
+//STEP18
+/*
+var x=20;
+alert (x+20)
+*/
+
+//STEP19
+/*
+var x=20
+alert (x*5)
+*/
+
+//STEP20
+/*
+var x=(20%3);
+alert (x)
+*/
+
+//STEP21
+
+/*
+var x=4;
+alert (x!=9)
+*/
+
+//STEP22
+/*
+var x=4;
+alert (x==9)
+*/
+
+//STEP23
+
+/*
+var widget = new Object();
+window.console.log(typeof widget);
+*/
+
+//STEP24
+
+/*
+window.console.log(widget instanceof Object);
+*/
+
+//STEP25
+/*
+window.console.log(widget instanceof Date);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
